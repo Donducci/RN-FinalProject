743a9f8b72e5f969ea44a815d23d48bc6fe2d678
Donducci <121743831+Donducci@users.noreply.github.com>
2023-11-30 09:43:09 -0500

    login UI

--- src/Screens/LoginScreen/index.js
+++ src/Screens/LoginScreen/index.js
@@ -1,9 +1,48 @@
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
 import {Text, TextInput, View, TouchableOpacity} from 'react-native';
 
 const LoginScreen = props => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'lightgray',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <TextInput
+        value={email}
+        onChangeText={text => {
+          setEmail(text);
+        }}
+        placeholder="enter email"
+        style={{
+          height: 40,
+          backgroundColor: 'lightgreen',
+          margin: 10,
+          borderRadius: 10,
+          borderWidth: 0.5,
+          width: 350,
+        }}
+      />
+      <TextInput
+        value={password}
+        onChangeText={text => {
+          setPassword(text);
+        }}
+        placeholder="enter password"
+        style={{
+          height: 40,
+          backgroundColor: 'lightgreen',
+          margin: 10,
+          borderRadius: 10,
+          borderWidth: 0.5,
+          width: 350,
+        }}
+      />
       <Text>Welcome to the LoginScreen</Text>
       <TouchableOpacity>
         <Text>Click me</Text>
--- src/Screens/SignupScreen/index.js
+++ src/Screens/SignupScreen/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, TextInput, View, TouchableOpacity} from 'react-native';
 
 const SignupScreen = props => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   return (
     <View>
       <Text>Welcome to the SignupScreen!</Text>
