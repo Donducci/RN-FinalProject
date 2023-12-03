f30cd4feed025c553c2ff08d78e597e2333a9ca4
Donducci <121743831+Donducci@users.noreply.github.com>
2023-12-03 03:23:46 -0600

    styles fixed

--- src/Screens/LoginScreen/index.js
+++ src/Screens/LoginScreen/index.js
@@ -1,32 +1,25 @@
-/* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {Text, TextInput, View, TouchableOpacity, Button} from 'react-native';
+import {
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  Button,
+  StyleSheet,
+} from 'react-native';
 
 const LoginScreen = props => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'lightgray',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
+    <View style={styles.view}>
       <TextInput
         value={email}
         onChangeText={text => {
           setEmail(text);
         }}
         placeholder="enter email"
-        style={{
-          height: 40,
-          backgroundColor: 'lightgreen',
-          margin: 10,
-          borderRadius: 10,
-          borderWidth: 0.5,
-          width: 350,
-        }}
+        style={styles.input}
       />
       <TextInput
         value={password}
@@ -34,14 +27,7 @@ const LoginScreen = props => {
           setPassword(text);
         }}
         placeholder="enter password"
-        style={{
-          height: 40,
-          backgroundColor: 'lightgreen',
-          margin: 10,
-          borderRadius: 10,
-          borderWidth: 0.5,
-          width: 350,
-        }}
+        style={styles.input}
       />
       <Button title={'Login'} onPress={async () => {}} />
       <Button
@@ -55,3 +41,20 @@ const LoginScreen = props => {
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    backgroundColor: 'lightgreen',
+    margin: 10,
+    borderRadius: 10,
+    borderWidth: 0.5,
+    width: 350,
+  },
+  view: {
+    flex: 1,
+    backgroundColor: 'lightgray',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
--- src/Screens/SignupScreen/index.js
+++ src/Screens/SignupScreen/index.js
@@ -1,19 +1,19 @@
-/* eslint-disable react/jsx-no-undef */
 /* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
-import {Text, TextInput, View, TouchableOpacity, Button} from 'react-native';
+import {
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  Button,
+  StyleSheet,
+} from 'react-native';
 
 const SignupScreen = props => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'lightgray',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
+    <View style={styles.view}>
       <TextInput
         value={email}
         onChangeText={text => {
@@ -35,14 +35,7 @@ const SignupScreen = props => {
           setPassword(text);
         }}
         placeholder="enter password"
-        style={{
-          height: 40,
-          backgroundColor: 'lightgreen',
-          margin: 10,
-          borderRadius: 10,
-          borderWidth: 0.5,
-          width: 350,
-        }}
+        style={styles.input}
       />
       <Button
         title={'Goto Login'}
@@ -55,3 +48,20 @@ const SignupScreen = props => {
 };
 
 export default SignupScreen;
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    backgroundColor: 'lightgreen',
+    margin: 10,
+    borderRadius: 10,
+    borderWidth: 0.5,
+    width: 350,
+  },
+  view: {
+    flex: 1,
+    backgroundColor: 'lightgray',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
