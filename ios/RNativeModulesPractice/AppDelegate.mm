d5588a84c7886fadeebcd70e1de5ad9e4fb0c640
Donducci <121743831+Donducci@users.noreply.github.com>
2023-12-13 02:10:14 -0500

    Map view control

--- ios/RNativeModulesPractice/AppDelegate.mm
+++ ios/RNativeModulesPractice/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 #import <Firebase.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -7,6 +8,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyCnvQ14JFFEacZiqZfNZTYwc3bH4olFpDw"];
   [FIRApp configure];
   
   self.moduleName = @"RNativeModulesPractice";
--- src/Screens/MapScreen/index.js
+++ src/Screens/MapScreen/index.js
@@ -1,36 +1,17 @@
+/* eslint-disable react/self-closing-comp */
 /* eslint-disable no-undef */
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import MapView from 'react-native-maps';
+import MapViewControl from '../../components/MapViewControl';
 
 const MapScreen = () => {
   return (
     <View style={{flex: 1}}>
-      <Text index />
-      <MapView
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-      />
+      <MapViewControl />
     </View>
   );
 };
 
 export default MapScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
+const styles = StyleSheet.create({});
